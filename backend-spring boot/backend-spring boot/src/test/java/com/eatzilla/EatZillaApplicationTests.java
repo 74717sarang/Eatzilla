892eefcec895bef7bcfcf550a78ea4687cef371e
74717sarang <patilsarang13520@gmail.com>
2024-10-11 17:23:28 +0530

    new Add backend Updated

---
+++ backend-spring boot/backend-spring boot/src/test/java/com/eatzilla/EatZillaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eatzilla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatZillaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
